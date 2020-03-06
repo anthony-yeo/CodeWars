7b0f264fbccdfb52e8dbaf741610c34422a8594b
antknees <47506456+antknees@users.noreply.github.com>
2020-03-06 09:03:10 -0800

    Update FindSquareRoot.java

--- InterviewCode/FindSquareRoot.java
+++ InterviewCode/FindSquareRoot.java
@@ -1,18 +1,7 @@
-package javarevisited.top.fifty.java.programs.from.coding.interviews;
+
 
 import java.math.BigDecimal;
 import java.util.Scanner;
-
-/**
- * @author medany
- */
-
-/*
- * You need to write a program to calculate the square root of a number without
- * using the Math.sqrt() function of JDK. You need to write your logic and
- * method to calculate the square root. You can though use popular algorithm
- * e.g. Newton's method.
- */
 
 public class FindSquareRoot {
 	public static void main(String args[]) {
