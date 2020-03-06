913775243dfdc846bbe49412176f10ce21620f20
antknees <47506456+antknees@users.noreply.github.com>
2020-03-06 09:02:00 -0800

    Update Factorial.java

--- InterviewCode/Factorial.java
+++ InterviewCode/Factorial.java
@@ -1,17 +1,8 @@
-package javarevisited.top.fifty.java.programs.from.coding.interviews;
+
 
 import java.math.BigInteger;
 import java.util.Scanner;
 
-/**
- * @author medany
- */
-
-/*
- * This is one of the simplest programs you can expect on interviews. It is
- * generally asked to see if you can code or not. Sometimes interviewer may also
- * ask about changing a recursive solution to iterative one or vice-versa.
- */
 public class Factorial {
 
 	public static void main(String[] args) {
