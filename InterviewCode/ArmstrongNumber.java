9385368b621aa9e20272d3faf0c8b81006f68621
antknees <47506456+antknees@users.noreply.github.com>
2020-03-06 09:01:34 -0800

    Update ArmstrongNumber.java

--- InterviewCode/ArmstrongNumber.java
+++ InterviewCode/ArmstrongNumber.java
@@ -1,17 +1,7 @@
-package javarevisited.top.fifty.java.programs.from.coding.interviews;
+
 
 import java.util.Scanner;
 
-/**
- * @author medany
- */
-
-/*
- * A number is called an Armstrong number if it is equal to the cube of its each
- * digit. for example, 153 is an Armstrong number because 153= 1+ 125+27 which
- * is equal to 1^3+5^3+3^3. You need to write a program to check if given number
- * is Armstrong number or not.
- */
 public class ArmstrongNumber {
 
 	public static void main(String[] args) {
